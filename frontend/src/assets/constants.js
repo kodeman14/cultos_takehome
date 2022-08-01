c3e901b87c26f795665eb45da423680d7daf414a
Ankit Curchorcar <ankit.curchorcar@gmail.com>
2022-08-01 13:35:09 -0400

    added assets files with constants and translatins

---
+++ frontend/src/assets/constants.js
@@ -0,0 +1,8 @@
+export const constants = {
+  pageSize: 5,
+  descMinLength: 5,
+  descMaxLength: 50,
+  pointsAmtMin: 10,
+  pointsAmtStep: 10,
+  pointsAmtMax: 1000,
+}
---
+++ frontend/src/assets/translations.js
@@ -0,0 +1,39 @@
+export const translations = {
+  yourActivityText: 'Your Activity',
+  totalEarnedText: 'Total Earned:',
+  editActivityText: 'Edit Activity',
+  createActivityText: 'Create Activity',
+  placeholders: {
+    thanksForText: 'Thanks for ',
+    detailsInputText: 'Enter a description',
+    platformSelectText: 'Select a platform',
+    typeSelectText: 'Select activity type',
+    deletePopupText: 'Row will be deleted, are you sure?',
+    cancelPopupText: 'Data will be erased, are you sure?',
+  },
+  colHeaders: {
+    dateCol: 'Date',
+    detailsCol: 'Description',
+    activityCol: 'Activity',
+    earnedCol: 'Earned',
+    actionsCol: 'Actions',
+  },
+  modalInputs: {
+    detailsLabel: 'Description:',
+    platformLabel: 'Social Platform:',
+    typeLabel: 'Social Type:',
+    pointsLabel: 'Points Label:',
+    cancelBtn: 'Cancel',
+    submitBtn: 'Confirm' ,
+  },
+  socialPlatform: {
+    twitter: 'Twitter',
+    facebook: 'Facebook',
+    instagram: 'Instagram',
+  },
+  socialType: {
+    liked: 'Liked',
+    shared: 'Shared',
+    posted: 'Posted',
+  }
+}
