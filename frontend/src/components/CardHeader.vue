a42816f98f1a0fbbd91b31142a2eb09cc2e99fe3
Ankit Curchorcar <ankit.curchorcar@gmail.com>
2022-08-03 12:28:00 -0400

    new file has template for header with props and emit

---
+++ frontend/src/components/CardHeader.vue
@@ -0,0 +1,34 @@
+<script setup>
+  import { constants } from '../assets/constants'
+  import { translations } from '../assets/translations'
+</script>
+
+<script>
+  export default {
+    props: ['isServerDown', 'totalPoints'],
+    emits: ['openModal']
+  }
+</script>
+
+<template #header>
+    <div class="card-header">
+      <el-row>
+        <el-col :span="18">
+          <el-row>
+            <el-col :span="9">
+              <h1 class="font-extrabold text-4xl">{{translations.yourActivityText}}</h1>
+            </el-col>
+            <el-col :span="9">
+              <h1 class="absolute top-2">
+                {{translations.totalEarnedText}}
+                <span :class="constants.gradientStyle.join(' ')">{{this.isServerDown ? constants.noPointsExist : this.totalPoints}}</span>
+              </h1>
+            </el-col>
+          </el-row>
+        </el-col>
+        <el-col :span="6">
+          <el-button class="absolute button right-0" type="primary" @click="this.$emit('openModal', false, {})">{{translations.createActivityText}}</el-button>
+        </el-col>
+      </el-row>
+    </div>
+</template>
