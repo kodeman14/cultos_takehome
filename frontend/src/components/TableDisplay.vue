ef4d945fdd20184d6a0e9a4b7c8d2a8a471daea7
Ankit Curchorcar <ankit.curchorcar@gmail.com>
2022-08-03 01:49:36 -0400

    new file has template for modal with props and emit

---
+++ frontend/src/components/TableDisplay.vue
@@ -0,0 +1,97 @@
+<script setup>
+  import { reactive, ref } from 'vue'
+
+  import { constants } from '../assets/constants'
+  import { translations } from '../assets/translations'
+  
+  const currPageRef = ref(constants.pageNum)
+  const pageSizeRef = ref(constants.pageSize)
+</script>
+
+<script>
+  export default {
+    props: ['pagedData', 'listLength', 'pageNum', 'pageSize'],
+    emits: ['editRow', 'deleteRow', 'sizingChange', 'pagingChange'],
+    methods: {
+      iconConvert(platform) {
+        switch(platform) {
+          case this.translations.socialPlatform.twitter:
+            return 'twitter'
+          case this.translations.socialPlatform.facebook:
+            return 'facebook'
+          case this.translations.socialPlatform.instagram:
+            return 'instagram'
+          default: //fail safe
+            return 'font-awesome'
+        }
+      },
+    }
+  }
+</script>
+
+<template>
+  <el-table
+    table-layout="auto"
+    class="cultos-table"
+    :data="this.pagedData"
+    header-cell-class-name="font-extrabold text-xl text-black"
+  >
+    <el-table-column :label="translations.colHeaders.dateCol" sortable prop="date">
+      <template #default="scope">
+        <p>{{scope.row.date.slice(0, 10)}}</p>
+      </template>
+    </el-table-column>
+    <el-table-column :label="translations.colHeaders.detailsCol">
+      <template #default="scope">
+        <p>{{translations.placeholders.thanksForText}} {{scope.row.description}}</p>
+      </template>
+    </el-table-column>
+    <el-table-column :label="translations.colHeaders.activityCol">
+      <template #default="scope">
+      <el-row>
+        <el-space>
+          <font-awesome-icon :icon="['fab', this.iconConvert(scope.row.socialPlatform)]" />
+          <p>{{scope.row.socialType}}</p>
+        </el-space>
+      </el-row>
+      </template>
+    </el-table-column>
+    <el-table-column :label="translations.colHeaders.earnedCol" sortable prop="pointsEarned">
+      <template #default="scope">
+        <p :class="constants.gradientStyle.join(' ')">+ {{scope.row.pointsEarned}}</p>
+      </template>
+    </el-table-column>
+    <el-table-column :label="translations.colHeaders.actionsCol">
+      <template #default="scope">
+        <el-button @click="this.$emit('editRow', scope.row)">
+          <font-awesome-icon icon="pen-to-square" />
+        </el-button>
+        <el-popconfirm
+          trigger="hover"
+          placement="right"
+          persistent="false"
+          :title="translations.placeholders.deletePopupText"
+          @confirm="this.$emit('deleteRow', scope.$index)"
+        >
+          <template #reference>
+            <el-button>
+              <font-awesome-icon icon="trash-can" />
+            </el-button>
+          </template>
+        </el-popconfirm>
+      </template>
+    </el-table-column>
+  </el-table>
+  <div class="flex justify-center mt-10">
+    <el-pagination
+      background
+      :total="listLength"
+      :page-sizes="[5, 10]"
+      v-model:page-size="pageSizeRef"
+      @size-change="this.$emit('sizingChange', value)"
+      v-model:currentPage="currPageRef"
+      @current-change="this.$emit('pagingChange', value)"
+      layout="total, sizes, prev, pager, next"
+    />
+  </div>
+</template>
