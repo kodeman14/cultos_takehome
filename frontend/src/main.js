1c7e7e2df9f190b476c892b12add114fe4b0a14e
Ankit Curchorcar <ankit.curchorcar@gmail.com>
2022-08-04 16:15:17 -0400

    basic routing and auth checking

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,9 +7,11 @@ import ElementPlus from 'element-plus'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faPenToSquare, faTrashCan } from '@fortawesome/free-solid-svg-icons'
-import { faTwitter, faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { faPenToSquare, faTrashCan, faBan } from '@fortawesome/free-solid-svg-icons'
+import { faNoteSticky, faThumbsUp, faShareSquare } from '@fortawesome/free-regular-svg-icons'
+import { faTwitter, faFacebook, faInstagram, faFortAwesome } from '@fortawesome/free-brands-svg-icons'
 
+import LoginForm from './components/LoginForm.vue'
 import CardHeader from './components/CardHeader.vue'
 import CreateModal from './components/CreateModal.vue'
 import TableDisplay from './components/TableDisplay.vue'
@@ -17,21 +19,30 @@ import TableDisplay from './components/TableDisplay.vue'
 import App from './App.vue'
 import './assets/main.css'
 import 'element-plus/dist/index.css'
+import router from './router'
 
+library.add(faBan);
 library.add(faTwitter);
-library.add(faFacebook);
 library.add(faTrashCan);
+library.add(faThumbsUp);
+library.add(faFacebook);
 library.add(faInstagram);
+library.add(faNoteSticky);
+library.add(faShareSquare);
 library.add(faPenToSquare);
+library.add(faFortAwesome);
 
 const app = createApp(App)
 
 app.component("font-awesome-icon", FontAwesomeIcon)
+  .component('LoginForm', LoginForm)
   .component('CardHeader', CardHeader)
   .component('CreateModal', CreateModal)
   .component('TableDisplay', TableDisplay)
+
 app.use(VueAxios, axios)
 app.use(createPinia())
 app.use(ElementPlus)
+app.use(router)
 
 app.mount('#app')
