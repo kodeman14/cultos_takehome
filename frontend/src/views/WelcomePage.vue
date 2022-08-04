78a7de2e88c957420a877873006827a750f10c56
Ankit Curchorcar <ankit.curchorcar@gmail.com>
2022-08-04 16:23:38 -0400

    welcome page on app load

---
+++ frontend/src/views/WelcomePage.vue
@@ -0,0 +1,25 @@
+<script setup>
+  import { useAuthStore } from '../stores/auth'
+  import LoginForm from '../components/LoginForm.vue'
+
+  const authStore = useAuthStore()
+</script>
+<script>
+  export default {
+    name: 'WelcomePage',
+    methods: {
+      appLogin(formData) {
+        console.log('user creds', formData)
+        this.authStore.loginApp(formData)
+      },
+    }
+  }
+</script>
+
+
+<template>
+  <main>
+    <h1 class="font-extrabold text-4xl">HELLO THERE</h1>
+    <LoginForm @app-login="this.appLogin" />
+  </main>
+</template>
