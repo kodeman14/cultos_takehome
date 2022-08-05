6a0acd41d663c9faade0adad3e3a3e7b348fd521
Ankit Curchorcar <ankit.curchorcar@gmail.com>
2022-08-05 01:54:58 -0400

    improved create modal design for mobile view

--- frontend/src/components/CreateModal.vue
+++ frontend/src/components/CreateModal.vue
@@ -39,13 +39,37 @@
   export default {
     name: 'CreateModal',
     props: ['isVisible', 'editFlag', 'activityForm'],
-    emits: ['createRow', 'closeModal']
+    emits: ['createRow', 'closeModal'],
+    data() {
+      return {
+        mobileWidth: window.innerWidth < constants.mobileWidth
+      }
+    },
+    methods: {
+      onResize(e) {
+        console.log('width', window.innerWidth)
+        this.mobileWidth = window.innerWidth < constants.mobileWidth
+      }
+    },
+    created() {
+      window.addEventListener("resize", this.onResize);
+    },
+    destroyed() {
+      window.removeEventListener("resize", this.onResize);
+    },
   }
 </script>
 
 <template>
   <!-- dialog component -->
-  <el-dialog v-model="isVisible" :title="translations[(editFlag ? 'edit' : 'create') + 'ActivityText']">
+  <el-dialog
+    :width="this.mobileWidth ? '75%' : '40%'"
+    v-model="isVisible"
+    :title="translations[
+      (editFlag ? 'edit' : 'create')
+      + 'ActivityText'
+    ]"
+  >
     <el-form
       ref="ruleFormRef"
       :rules="inputRules"
@@ -53,33 +77,42 @@
       :model="activityForm"
     >
       <el-form-item :label="translations.modalInputs.detailsLabel" prop="description">
-        <el-col :span="18">
-          <el-input
-            clearable
-            type="text"
-            v-model="activityForm.description"
-            :minLength="constants.descMinLength"
-            :maxLength="constants.descMaxLength"
-            :placeholder="translations.placeholders.detailsInputText"
-          >
-            <template #prepend>
-              <span>{{translations.placeholders.thanksForText}}</span>
-            </template>
-            <template #append>
-              <span>{{ constants.descMaxLength - activityForm.description.length}} / {{ constants.descMaxLength }}</span>
-            </template>
-          </el-input>
-        </el-col>
+        <el-input
+          clearable
+          type="text"
+          class="w-full"
+          v-model="activityForm.description"
+          :minLength="constants.descMinLength"
+          :maxLength="constants.descMaxLength"
+          :placeholder="translations.placeholders.detailsInputText"
+        >
+          <template #prepend v-if="!this.mobileWidth">
+            <span>{{translations.placeholders.thanksForText}}</span>
+          </template>
+          <template #append v-if="!this.mobileWidth">
+            <span>{{ constants.descMaxLength - activityForm.description.length}} / {{ constants.descMaxLength }}</span>
+          </template>
+        </el-input>
       </el-form-item>
       <el-form-item :label="translations.modalInputs.platformLabel" prop="socialPlatform">
-        <el-select v-model="activityForm.socialPlatform" clearable :placeholder="translations.placeholders.platformSelectText">
+        <el-select
+          clearable
+          class="w-full"
+          v-model="activityForm.socialPlatform"
+          :placeholder="translations.placeholders.platformSelectText"
+        >
           <el-option :label="translations.socialPlatform.twitter" :value="translations.socialPlatform.twitter" />
           <el-option :label="translations.socialPlatform.facebook" :value="translations.socialPlatform.facebook" />
           <el-option :label="translations.socialPlatform.instagram" :value="translations.socialPlatform.instagram" />
         </el-select>
       </el-form-item>
       <el-form-item :label="translations.modalInputs.typeLabel" prop="socialType">
-        <el-select v-model="activityForm.socialType" clearable :placeholder="translations.placeholders.typeSelectText">
+        <el-select
+          clearable
+          class="w-full"
+          v-model="activityForm.socialType"
+          :placeholder="translations.placeholders.typeSelectText"
+        >
           <el-option :label="translations.socialType.liked" :value="translations.socialType.liked" />
           <el-option :label="translations.socialType.shared" :value="translations.socialType.shared" />
           <el-option :label="translations.socialType.posted" :value="translations.socialType.posted" />
@@ -88,6 +121,7 @@
       <el-form-item :label="translations.modalInputs.pointsLabel" prop="pointsEarned">
         <el-input-number
           type="num"
+          class="w-full"
           :max="constants.pointsAmtMax"
           :min="constants.pointsAmtStep"
           :step="constants.pointsAmtStep"
@@ -109,7 +143,7 @@
             <el-button>{{translations.modalInputs.cancelBtn}}</el-button>
           </template>
         </el-popconfirm>
-        <el-button type="primary" @click="this.$emit('createRow', this.activityForm, ruleFormRef)">
+        <el-button @click="this.$emit('createRow', this.activityForm, ruleFormRef)">
           {{translations.modalInputs.submitBtn}}
         </el-button>
       </span>
