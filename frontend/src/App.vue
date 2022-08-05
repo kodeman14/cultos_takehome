01dc525e2e00fde9d82809c49ccd80e6da45f9b5
Ankit Curchorcar <ankit.curchorcar@gmail.com>
2022-08-05 03:09:16 -0400

    removed excess imports

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,5 @@
 <script setup>
   import { RouterView } from 'vue-router'
-  import { reactive, isProxy, toRaw } from 'vue'
-  import { ElMessage, ElTable } from 'element-plus'
-
-  import { constants } from './assets/constants'
-  import { translations } from './assets/translations'
 </script>
 
 <template>
