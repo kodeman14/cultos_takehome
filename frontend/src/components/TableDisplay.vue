b49869a1c42ec0f399908175e9f965660a03dbe9
Ankit Curchorcar <ankit.curchorcar@gmail.com>
2022-08-08 17:04:49 -0400

    removed scrollbar, added height to table itself

--- frontend/src/components/TableDisplay.vue
+++ frontend/src/components/TableDisplay.vue
@@ -77,111 +77,110 @@
 </script>
 
 <template>
-  <el-scrollbar max-height="500px">
-    <el-table
-      :data="this.pagedData"
-      v-loading="this.loadingRef"
-      :element-loading-text="translations.loadingText"
-      :table-layout="this.mobileWidth ? 'fixed': 'auto'"
-      header-cell-class-name="lg:font-extrabold lg:text-xl text-black"
+  <el-table
+    max-height="500px"
+    :data="this.pagedData"
+    v-loading="this.loadingRef"
+    :element-loading-text="translations.loadingText"
+    :table-layout="this.mobileWidth ? 'fixed': 'auto'"
+    header-cell-class-name="lg:font-extrabold lg:text-xl text-black"
+  >
+    <!-- date -->
+    <el-table-column
+      prop="date"
+      align="center"
+      header-align="center"
+      v-if="!this.mobileWidth"
+      :sortable="!this.mobileWidth"
+      :label="translations.colHeaders.dateCol"
     >
-      <!-- date -->
-      <el-table-column
-        prop="date"
-        align="center"
-        header-align="center"
-        v-if="!this.mobileWidth"
-        :sortable="!this.mobileWidth"
-        :label="translations.colHeaders.dateCol"
-      >
-        <template #default="scope">
-          <p>{{scope.row.date.slice(0, 10)}}</p>
-        </template>
-      </el-table-column>
+      <template #default="scope">
+        <p>{{scope.row.date.slice(0, 10)}}</p>
+      </template>
+    </el-table-column>
 
-      <!-- details -->
-      <el-table-column
-        align="center"
-        prop="description"
-        header-align="center"
-        :label="translations.colHeaders.detailsCol"
-      >
-        <template #default="scope">
-          <p>
-            <span v-if="!this.mobileWidth">{{translations.placeholders.thanksForText}}</span>
-            {{scope.row.description}}
-          </p>
-        </template>
-      </el-table-column>
+    <!-- details -->
+    <el-table-column
+      align="center"
+      prop="description"
+      header-align="center"
+      :label="translations.colHeaders.detailsCol"
+    >
+      <template #default="scope">
+        <p>
+          <span v-if="!this.mobileWidth">{{translations.placeholders.thanksForText}}</span>
+          {{scope.row.description}}
+        </p>
+      </template>
+    </el-table-column>
 
-      <!-- platform -->
-      <el-table-column
-        align="center"
-        header-align="center"
-        :filters="filterOptions"
-        filter-placement="right-start"
-        :filter-method="filterHandler"
-        :filter-multiple="false"
-        :label="translations.colHeaders.activityCol"
-      >
-        <template #default="scope">
-          <div class="flex justify-center">
-            <el-space>
-              <font-awesome-icon :icon="['fab', this.iconPlatformConvert(scope.row.socialPlatform)]" />
-              <div v-if="this.mobileWidth">
-                <font-awesome-icon :icon="['far', this.iconActivityConvert(scope.row.socialType)]" />
-              </div>
-              <div v-else>
-                <p>{{scope.row.socialType}}</p>
-              </div>
-            </el-space>
-          </div>
-        </template>
-      </el-table-column>
-
-      <!-- points -->
-      <el-table-column
-        align="center"
-        prop="pointsEarned"
-        header-align="center"
-        :sortable="!this.mobileWidth"
-        :label="translations.colHeaders.earnedCol"
-      >
-        <template #default="scope">
-          <p :class="constants.gradientStyle.join(' ')">
-            + {{scope.row.pointsEarned}}
-          </p>
-        </template>
-      </el-table-column>
-
-      <!-- actions -->
-      <el-table-column
-        align="center"
-        header-align="center"
-        :label="translations.colHeaders.actionsCol"
-      >
-        <template #default="scope">
-          <el-space :fill="fill" wrap>
-            <font-awesome-icon
-              icon="pen-to-square"
-              @click="this.$emit('editRow', scope.row)"
-            />
-            <el-popconfirm
-              trigger="click"
-              placement="right"
-              persistent="false"
-              @confirm="this.$emit('deleteRow', scope.$index)"
-              :title="translations.placeholders.deletePopupText"
-            >
-              <template #reference>
-                <font-awesome-icon icon="trash-can" />
-              </template>
-            </el-popconfirm>
+    <!-- platform -->
+    <el-table-column
+      align="center"
+      header-align="center"
+      :filters="filterOptions"
+      filter-placement="right-start"
+      :filter-method="filterHandler"
+      :filter-multiple="false"
+      :label="translations.colHeaders.activityCol"
+    >
+      <template #default="scope">
+        <div class="flex justify-center">
+          <el-space>
+            <font-awesome-icon :icon="['fab', this.iconPlatformConvert(scope.row.socialPlatform)]" />
+            <div v-if="this.mobileWidth">
+              <font-awesome-icon :icon="['far', this.iconActivityConvert(scope.row.socialType)]" />
+            </div>
+            <div v-else>
+              <p>{{scope.row.socialType}}</p>
+            </div>
           </el-space>
-        </template>
-      </el-table-column>
-    </el-table>
-  </el-scrollbar>
+        </div>
+      </template>
+    </el-table-column>
+
+    <!-- points -->
+    <el-table-column
+      align="center"
+      prop="pointsEarned"
+      header-align="center"
+      :sortable="!this.mobileWidth"
+      :label="translations.colHeaders.earnedCol"
+    >
+      <template #default="scope">
+        <p :class="constants.gradientStyle.join(' ')">
+          + {{scope.row.pointsEarned}}
+        </p>
+      </template>
+    </el-table-column>
+
+    <!-- actions -->
+    <el-table-column
+      align="center"
+      header-align="center"
+      :label="translations.colHeaders.actionsCol"
+    >
+      <template #default="scope">
+        <el-space :fill="fill" wrap>
+          <font-awesome-icon
+            icon="pen-to-square"
+            @click="this.$emit('editRow', scope.row)"
+          />
+          <el-popconfirm
+            trigger="click"
+            placement="right"
+            persistent="false"
+            @confirm="this.$emit('deleteRow', scope.$index)"
+            :title="translations.placeholders.deletePopupText"
+          >
+            <template #reference>
+              <font-awesome-icon icon="trash-can" />
+            </template>
+          </el-popconfirm>
+        </el-space>
+      </template>
+    </el-table-column>
+  </el-table>
 
   <!-- pagination -->
   <div class="flex justify-center mt-10">
