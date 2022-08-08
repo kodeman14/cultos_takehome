6c64fbd229bc33ac8b57ce899d7b2c5befea20c2
Ankit Curchorcar <ankit.curchorcar@gmail.com>
2022-08-08 12:49:56 -0400

    added filter for activity column

--- frontend/src/components/TableDisplay.vue
+++ frontend/src/components/TableDisplay.vue
@@ -8,6 +8,15 @@
   const pageSizeRef = ref(constants.pageSize)
   const mobilePageLayout = "prev, pager, next"
   const desktopPageLayout = `total, sizes, ${mobilePageLayout}, jumper`
+
+  const filterOptions = [
+    { text: translations.socialType.liked, value: translations.socialType.liked },
+    { text: translations.socialType.shared, value: translations.socialType.shared },
+    { text: translations.socialType.posted, value: translations.socialType.posted },
+    { text: translations.socialPlatform.twitter, value: translations.socialPlatform.twitter },
+    { text: translations.socialPlatform.facebook, value: translations.socialPlatform.facebook },
+    { text: translations.socialPlatform.instagram, value: translations.socialPlatform.instagram },
+  ]
 </script>
 
 <script>
@@ -30,11 +39,11 @@
     methods: {
       iconPlatformConvert(platform) {
         switch(platform) {
-          case this.translations.socialPlatform.twitter:
+          case translations.socialPlatform.twitter:
             return 'twitter'
-          case this.translations.socialPlatform.facebook:
+          case translations.socialPlatform.facebook:
             return 'facebook'
-          case this.translations.socialPlatform.instagram:
+          case translations.socialPlatform.instagram:
             return 'instagram'
           default: //fail safe
             return 'fort-awesome'
@@ -42,11 +51,11 @@
       },
       iconActivityConvert(activity) {
         switch(activity) {
-          case this.translations.socialType.liked:
+          case translations.socialType.liked:
             return 'thumbs-up'
-          case this.translations.socialType.shared:
+          case translations.socialType.shared:
             return 'share-square'
-          case this.translations.socialType.posted:
+          case translations.socialType.posted:
             return 'note-sticky'
           default: //fail safe
             return 'ban'
@@ -61,111 +70,119 @@
       onResize(e) {
         console.log('width', window.innerWidth)
         this.mobileWidth = window.innerWidth < constants.mobileWidth
-      }
+      },
+      filterHandler(value, row) {
+        return row.socialPlatform === value || row.socialType === value
+      },
     },
   }
 </script>
 
 <template>
   <el-scrollbar max-height="500px">
-  <el-table
-    :data="this.pagedData"
-    v-loading="this.loadingRef"
-    :element-loading-text="translations.loadingText"
-    :table-layout="this.mobileWidth ? 'fixed': 'auto'"
-    header-cell-class-name="lg:font-extrabold lg:text-xl text-black"
-  >
-    <!-- date -->
-    <el-table-column
-      prop="date"
-      align="center"
-      header-align="center"
-      v-if="!this.mobileWidth"
-      :sortable="!this.mobileWidth"
-      :label="translations.colHeaders.dateCol"
+    <el-table
+      :data="this.pagedData"
+      v-loading="this.loadingRef"
+      :element-loading-text="translations.loadingText"
+      :table-layout="this.mobileWidth ? 'fixed': 'auto'"
+      header-cell-class-name="lg:font-extrabold lg:text-xl text-black"
     >
-      <template #default="scope">
-        <p>{{scope.row.date.slice(0, 10)}}</p>
-      </template>
-    </el-table-column>
-    
-    <!-- details -->
-    <el-table-column 
-      align="center"
-      header-align="center"
-      :label="translations.colHeaders.detailsCol"
-    >
-      <template #default="scope">
-        <p>
-          <span v-if="!this.mobileWidth">{{translations.placeholders.thanksForText}}</span>
-          {{scope.row.description}}
-        </p>
-      </template>
-    </el-table-column>
+      <!-- date -->
+      <el-table-column
+        prop="date"
+        align="center"
+        header-align="center"
+        v-if="!this.mobileWidth"
+        :sortable="!this.mobileWidth"
+        :label="translations.colHeaders.dateCol"
+      >
+        <template #default="scope">
+          <p>{{scope.row.date.slice(0, 10)}}</p>
+        </template>
+      </el-table-column>
 
-    <!-- platform -->
-    <el-table-column
-      align="center"
-      header-align="center"
-      :label="translations.colHeaders.activityCol"
-    >
-      <template #default="scope">
-        <div class="flex justify-center">
-          <el-space>
-            <font-awesome-icon :icon="['fab', this.iconPlatformConvert(scope.row.socialPlatform)]" />
-            <div v-if="this.mobileWidth">
-              <font-awesome-icon :icon="['far', this.iconActivityConvert(scope.row.socialType)]" />
-            </div>
-            <div v-else>
-              <p>{{scope.row.socialType}}</p>
-            </div>
+      <!-- details -->
+      <el-table-column
+        align="center"
+        prop="description"
+        header-align="center"
+        :label="translations.colHeaders.detailsCol"
+      >
+        <template #default="scope">
+          <p>
+            <span v-if="!this.mobileWidth">{{translations.placeholders.thanksForText}}</span>
+            {{scope.row.description}}
+          </p>
+        </template>
+      </el-table-column>
+
+      <!-- platform -->
+      <el-table-column
+        align="center"
+        header-align="center"
+        :filters="filterOptions"
+        filter-placement="right-start"
+        :filter-method="filterHandler"
+        :filter-multiple="false"
+        :label="translations.colHeaders.activityCol"
+      >
+        <template #default="scope">
+          <div class="flex justify-center">
+            <el-space>
+              <font-awesome-icon :icon="['fab', this.iconPlatformConvert(scope.row.socialPlatform)]" />
+              <div v-if="this.mobileWidth">
+                <font-awesome-icon :icon="['far', this.iconActivityConvert(scope.row.socialType)]" />
+              </div>
+              <div v-else>
+                <p>{{scope.row.socialType}}</p>
+              </div>
+            </el-space>
+          </div>
+        </template>
+      </el-table-column>
+
+      <!-- points -->
+      <el-table-column
+        align="center"
+        prop="pointsEarned"
+        header-align="center"
+        :sortable="!this.mobileWidth"
+        :label="translations.colHeaders.earnedCol"
+      >
+        <template #default="scope">
+          <p :class="constants.gradientStyle.join(' ')">
+            + {{scope.row.pointsEarned}}
+          </p>
+        </template>
+      </el-table-column>
+
+      <!-- actions -->
+      <el-table-column
+        align="center"
+        header-align="center"
+        :label="translations.colHeaders.actionsCol"
+      >
+        <template #default="scope">
+          <el-space :fill="fill" wrap>
+            <font-awesome-icon
+              icon="pen-to-square"
+              @click="this.$emit('editRow', scope.row)"
+            />
+            <el-popconfirm
+              trigger="click"
+              placement="right"
+              persistent="false"
+              @confirm="this.$emit('deleteRow', scope.$index)"
+              :title="translations.placeholders.deletePopupText"
+            >
+              <template #reference>
+                <font-awesome-icon icon="trash-can" />
+              </template>
+            </el-popconfirm>
           </el-space>
-        </div>
-      </template>
-    </el-table-column>
-
-    <!-- points -->
-    <el-table-column
-      align="center"
-      prop="pointsEarned"
-      header-align="center"
-      :sortable="!this.mobileWidth"
-      :label="translations.colHeaders.earnedCol"
-    >
-      <template #default="scope">
-        <p :class="constants.gradientStyle.join(' ')">
-          + {{scope.row.pointsEarned}}
-        </p>
-      </template>
-    </el-table-column>
-
-    <!-- actions -->
-    <el-table-column
-      align="center"
-      header-align="center"
-      :label="translations.colHeaders.actionsCol"
-    >
-      <template #default="scope">
-        <el-space :fill="fill" wrap>
-          <font-awesome-icon
-            icon="pen-to-square"
-            @click="this.$emit('editRow', scope.row)"
-          />
-          <el-popconfirm
-            trigger="click"
-            placement="right"
-            persistent="false"
-            @confirm="this.$emit('deleteRow', scope.$index)"
-            :title="translations.placeholders.deletePopupText"
-          >
-            <template #reference>
-              <font-awesome-icon icon="trash-can" />
-            </template>
-          </el-popconfirm>
-        </el-space>
-      </template>
-    </el-table-column>
-  </el-table>
+        </template>
+      </el-table-column>
+    </el-table>
   </el-scrollbar>
 
   <!-- pagination -->
@@ -173,7 +190,7 @@
     <el-pagination
       background
       :total="listLength"
-      :page-sizes="[5, 10, 20, 40]"
+      :page-sizes="[10, 20, 40, 80]"
       v-model:page-size="pageSizeRef"
       @size-change="this.handleSizing"
       v-model:currentPage="currPageRef"
@@ -183,9 +200,3 @@
     />
   </div>
 </template>
-
-<style>
-  .el-table th .cell {
-    white-space: nowrap !important;
-  }
-</style>
